a950072df487efb04aed1449d1e51248dd4af8d8
ambushwork <378248322@qq.com>
2019-07-17 04:02:02 +0800

    Configure FFmpeg build enviroment

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,14 @@
 #include <jni.h>
 #include <string>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_netatmo_ylu_ffmpegplayer_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF(av_version_info());
 }
